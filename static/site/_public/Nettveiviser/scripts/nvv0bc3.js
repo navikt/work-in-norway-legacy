929a24214151bbfff4b88df9c9b0ef37b01cc9f3
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-02-13 09:16:55 +0100

    Legger til skiplink

--- static/site/_public/Nettveiviser/scripts/nvv0bc3.js
+++ static/site/_public/Nettveiviser/scripts/nvv0bc3.js
@@ -1,37 +1,47 @@
-$(document).ready(function() {
-	dropDownShortcuts();
+$(document).ready(function () {
+    dropDownShortcuts();
 });
 
+function scrollToMain(e) {
+    if (e) {
+        e.preventDefault();
+    }
+    const mainContent = document.getElementById('maincontent');
+    if (mainContent) {
+        mainContent.focus();
+    }
+}
+
 function dropDownShortcuts() {
-	if ($(".dropdown-1")) {
-		doDynamicWidth();
+    if ($('.dropdown-1')) {
+        doDynamicWidth();
 
-		$(".dropdown-1").mouseleave(function(e) {
-			e.preventDefault();
+        $('.dropdown-1').mouseleave(function (e) {
+            e.preventDefault();
 
-			$(".dropdown-1").slideToggle();
-		});
+            $('.dropdown-1').slideToggle();
+        });
 
-		$(".dropdown-area").click(function(e) {
-			e.preventDefault();
+        $('.dropdown-area').click(function (e) {
+            e.preventDefault();
 
-			$(".dropdown-1").slideToggle();
-		});
+            $('.dropdown-1').slideToggle();
+        });
 
-		$(document).mouseup(function(e) {
-			var container = $(".dropdown-1");
+        $(document).mouseup(function (e) {
+            var container = $('.dropdown-1');
 
-			if (container.has(e.target).length == 0) {
-				container.slideUp();
-			}
-		});
-	}
+            if (container.has(e.target).length == 0) {
+                container.slideUp();
+            }
+        });
+    }
 }
 
 function doDynamicWidth() {
-	var width = $(".dropdown-area").width();
+    var width = $('.dropdown-area').width();
 
-	$(".dropdown-1").css("width", width);
+    $('.dropdown-1').css('width', width);
 }
 
 /**
@@ -40,32 +50,32 @@ function doDynamicWidth() {
  * med at sidene ble gjort om til å være responsive.
  */
 
-(function() {
-	var BREAKPOINT = 1030; // Breakpoint som brukes i css
-	var scrollToElementId = "js-scroll-innholdContainer";
-	var forsideNorsk = "/no/Forside";
-	var forsideEngelsk = "/en/Home";
+(function () {
+    var BREAKPOINT = 1030; // Breakpoint som brukes i css
+    var scrollToElementId = 'js-scroll-innholdContainer';
+    var forsideNorsk = '/no/Forside';
+    var forsideEngelsk = '/en/Home';
 
-	function brukerKommerFraEnUnderside() {
-		return (
-			document.referrer.indexOf(forsideNorsk) === -1 &&
-			document.referrer.indexOf(forsideEngelsk) === -1
-		);
-	}
+    function brukerKommerFraEnUnderside() {
+        return (
+            document.referrer.indexOf(forsideNorsk) === -1 &&
+            document.referrer.indexOf(forsideEngelsk) === -1
+        );
+    }
 
-	document.addEventListener("DOMContentLoaded", function(event) {
-		// Scroll til innholdet dersom det er responsiv visning og
-		// bruker ikke kommer fra en underside. Når bruker kommer fra en forside
-		// er det greit å vise menyen først.
+    document.addEventListener('DOMContentLoaded', function (event) {
+        // Scroll til innholdet dersom det er responsiv visning og
+        // bruker ikke kommer fra en underside. Når bruker kommer fra en forside
+        // er det greit å vise menyen først.
 
-		if (window.innerWidth <= BREAKPOINT && brukerKommerFraEnUnderside()) {
-			content = document.getElementById(scrollToElementId);
-			if (content && content.getBoundingClientRect) {
-				setTimeout(function() {
-					pos = content.getBoundingClientRect();
-					window.scrollTo(0, pos.top + window.scrollY - 30);
-				}, 10);
-			}
-		}
-	});
+        if (window.innerWidth <= BREAKPOINT && brukerKommerFraEnUnderside()) {
+            content = document.getElementById(scrollToElementId);
+            if (content && content.getBoundingClientRect) {
+                setTimeout(function () {
+                    pos = content.getBoundingClientRect();
+                    window.scrollTo(0, pos.top + window.scrollY - 30);
+                }, 10);
+            }
+        }
+    });
 })();
